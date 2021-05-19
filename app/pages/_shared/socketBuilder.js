4e5a5001d864c4b965df0b07dca6656c95603a32
WeslleyFratini <weeslleey354@gmail.com>
2021-05-19 19:05:30 -0300

    finish 2 class

--- app/pages/_shared/socketBuilder.js
+++ app/pages/_shared/socketBuilder.js
@@ -1,36 +1,35 @@
-import { constants } from "./constants.js"
-
+import { constants } from "./constants.js";
 
 export default class SocketBuilder {
   constructor({ socketUrl, namespace }) {
-    this.socketUrl = `${socketUrl}/${namespace}`
+    this.socketUrl = `${socketUrl}/${namespace}`;
 
-    this.onUserConnected = () => { }
-    this.onUserDisconnected = () => { }
+    this.onUserConnected = () => {};
+    this.onUserDisconnected = () => {};
   }
 
   setOnUserConnected(fn) {
-    this.onUserConnected = fn
+    this.onUserConnected = fn;
 
-    return this
+    return this;
   }
 
   setOnUserDisconnected(fn) {
-    this.onUserDisconnected = fn
+    this.onUserDisconnected = fn;
 
-    return this
+    return this;
   }
 
   build() {
     const socket = globalThis.io.connect(this.socketUrl, {
-      withCredentials: false
-    })
+      withCredentials: false,
+    });
 
-    socket.on('connection', () => console.log('conectei!'))
+    socket.on("connect", () => console.log("conectei!"));
 
-    socket.on(constants.events.USER_CONNECTED, this.onUserConnected)
-    socket.on(constants.events.USER_DISCONNECTED, this.onUserDisconnected)
+    socket.on(constants.events.USER_CONNECTED, this.onUserConnected);
+    socket.on(constants.events.USER_DISCONNECTED, this.onUserDisconnected);
 
-    return socket
+    return socket;
   }
 }
--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,25 +1,31 @@
 import { constants } from "../../_shared/constants.js";
-import RoomSocketBuilder from './util/roomSocket'
+import RoomController from "./controller.js";
+import RoomSocketBuilder from "./util/roomSocket.js";
+import View from "./view.js";
 
-const socket = new SocketBuilder({
+const urlParams = new URLSearchParams(window.location.search);
+const keys = ["id", "topic"];
+const urlData = keys.map((key) => [key, urlParams.get(key)]);
+
+const user = {
+  img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png",
+  username: "Erick " + Date.now(),
+};
+
+const roomInfo = {
+  room: { ...Object.fromEntries(urlData) },
+  user,
+};
+
+const socketBuilder = new RoomSocketBuilder({
   socketUrl: constants.socketUrl,
   namespace: constants.socketNamespaces.room,
 });
 
-const socket = SocketBuilder
-  .setOnUserConnected((user) = console.log("user connected!", user))
-  .setOnUserDisconnected((user) = console.log("user disconnected!", user))
-  .setOnUserUpdated((room) => console.log('room list!', room))
-  .build()
-
-const room = {
-  id: '0001',
-  topic: "Js expert"
+const dependencies = {
+  view: View,
+  socketBuilder,
+  roomInfo,
 };
 
-const user = {
-  img: "https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-256.png",
-  username: "Weslley Fratini" + Date.now(),
-};
-
-socket.emit(constants.events.JOIN_ROOM, { user, room });
+await RoomController.initialize(dependencies);
