020ca95ef379decceff321d5f28c21667e477e76
WeslleyFratini <weeslleey354@gmail.com>
2021-05-21 19:45:43 -0300

    chore: update pages lobby

--- app/pages/lobby/src/entities/room.js
+++ app/pages/lobby/src/entities/room.js
@@ -1,8 +1,8 @@
 class Attendee {
   constructor({ id, img, username }) {
-    this.id = id
-    this.img = img
-    this.username = username
+    this.id = id;
+    this.img = img;
+    this.username = username;
   }
 }
 
@@ -17,14 +17,15 @@ export default class Room {
     featuredAttendees,
     owner,
   }) {
-
-    this.id = id
-    this.topic = topic
-    this.subTopic = subTopic || "Semana JS Expert 4.0"
-    this.attendeesCount = attendeesCount
-    this.speakersCount = speakersCount
-    this.featuredAttendees = featuredAttendees?.map(attendee => new Attendee(attendee))
-    this.owner = new Attendee(owner)
-    this.roomLink = roomLink
+    this.id = id;
+    this.topic = topic;
+    this.subTopic = subTopic || "Semana JS Expert 4.0";
+    this.attendeesCount = attendeesCount;
+    this.speakersCount = speakersCount;
+    this.featuredAttendees = featuredAttendees?.map(
+      (attendee) => new Attendee(attendee)
+    );
+    this.owner = new Attendee(owner);
+    this.roomLink = roomLink;
   }
 }
--- app/pages/lobby/src/view.js
+++ app/pages/lobby/src/view.js
@@ -1,55 +1,65 @@
-import Room from "./entities/room.js"
-import getTemplate from "./templates/lobbyItem.js"
+import { constants } from "../../_shared/constants.js";
+import Room from "./entities/room.js";
+import getTemplate from "./templates/lobbyItem.js";
 
-const roomGrid = document.getElementById('roomGrid')
+const roomGrid = document.getElementById("roomGrid");
 
-const btnCreateRoomWithTopic = document.getElementById('btnCreateRoomWithTopic')
-const btnCreateRoomWithoutTopic = document.getElementById('btnCreateRoomWithoutTopic')
-const txtTopic = document.getElementById('txtTopic')
+const btnCreateRoomWithTopic = document.getElementById(
+  "btnCreateRoomWithTopic"
+);
+const btnCreateRoomWithoutTopic = document.getElementById(
+  "btnCreateRoomWithoutTopic"
+);
+const txtTopic = document.getElementById("txtTopic");
+const imgUser = document.getElementById("imgUser");
 
 export default class View {
   static clearRoomList() {
-    roomGrid.innerHTML = ''
+    roomGrid.innerHTML = "";
   }
   static generateRoomLink({ id, topic }) {
-    return `./../room/index.html?id=${id}&topic=${topic}`
+    return `./../room/index.html?id=${id}&topic=${topic}`;
   }
 
-  static redirectToRoom(topic = '') {
+  static redirectToLogin() {
+    window.location = constants.pages.login;
+  }
+
+  static redirectToRoom(topic = "") {
     // https://stackoverflow.com/a/44078785/4087199
-    const id = Date.now().toString(36) + Math.random().toString(36).substring(2)
+    const id =
+      Date.now().toString(36) + Math.random().toString(36).substring(2);
     window.location = View.generateRoomLink({
       id,
-      topic
-    })
+      topic,
+    });
   }
 
   static configureCreateRoomButton() {
-    btnCreateRoomWithoutTopic.addEventListener('click', () => {
-      View.redirectToRoom()
-    })
-    btnCreateRoomWithTopic.addEventListener('click', () => {
-      const topic = txtTopic.value
-      View.redirectToRoom(topic)
-
-    })
+    btnCreateRoomWithoutTopic.addEventListener("click", () => {
+      View.redirectToRoom();
+    });
+    btnCreateRoomWithTopic.addEventListener("click", () => {
+      const topic = txtTopic.value;
+      View.redirectToRoom(topic);
+    });
   }
   static updateRoomList(rooms) {
-    View.clearRoomList()
+    View.clearRoomList();
 
-    rooms.forEach(room => {
+    rooms.forEach((room) => {
       const params = new Room({
         ...room,
-        roomLink: View.generateRoomLink(room)
-      })
-      const htmlTemplate = getTemplate(params)
+        roomLink: View.generateRoomLink(room),
+      });
+      const htmlTemplate = getTemplate(params);
 
-      roomGrid.innerHTML += htmlTemplate
-    })
+      roomGrid.innerHTML += htmlTemplate;
+    });
   }
 
   static updateUserImage({ img, username }) {
-    imgUser.src = img
-    imgUser.al = username
+    imgUser.src = img;
+    imgUser.al = username;
   }
 }
